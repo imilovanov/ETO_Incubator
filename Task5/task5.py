cdd78ef9c1a5d1ba49dafa98cca6475d3f0af307
Ivan Milovanov <imilovanov@protonmail.com>
2019-06-13 18:07:12 +0700

    Add Page Object

--- Task5/task5.py
+++ Task5/task5.py
@@ -1,56 +1,69 @@
 #!/usr/bin/env python
-
 from selenium import webdriver
 import unittest
 import time
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 
-# CASE 1
-browser = webdriver.Firefox()
-browser.get('https://rasp.yandex.ru')
-elem = browser.find_element_by_id('from')
-elem.clear()
-elem.send_keys('Кемерово')
-time.sleep(1)
-elem = browser.find_element_by_id('to')
-elem.send_keys('Москва')
-elem = browser.find_element_by_id('when')
-elem.send_keys('7 июля')
-elem.send_keys(Keys.ENTER)
-time.sleep(4)
-elem = browser.find_element_by_xpath("//html/body/div/div/main/div/div[1]/div[1]/div/section/article[1]/header/div/h3/a").text
-print(elem)
-elem = browser.find_element_by_xpath("//html/body/div/div/main/div/div[1]/div[1]/div/section/article[1]/div[1]/div[1]/div[2]").text
-print(elem)
-time.sleep(4)
-try:
-    browser.find_element_by_tag_name('svg')
-except NoSuchElementException:
-    print('No icon')
-elements = len(browser.find_elements_by_class_name('SearchSegment'))
-if elements < 5:
-    print('False')
-browser.quit()
+class SchedulePage(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.get("https://rasp.yandex.ru")
 
-# CASE 2
-browser = webdriver.Firefox()
-browser.get('https://rasp.yandex.ru')
-elem = browser.find_element_by_id('from')
-elem.clear()
-time.sleep(2)
-elem.send_keys('Кемерово проспект Ленина')
-elem = browser.find_element_by_id('to')
-elem.send_keys('Кемерово Бакинский переулок')
-elem = browser.find_element_by_id('when')
-elem.send_keys('среда')
-bus = browser.find_element_by_xpath(
-    '//*[@id="root"]/div/header/div[1]/div/div[4]/span/label[5]')
-bus.click()
-time.sleep(2)
-button = browser.find_element_by_xpath('//*[@id="root"]/div/header/div[1]/div/div[5]/form/button[2]')
-button.click()
-try:
-    browser.find_elements_by_class_name('ErrorPage')
-except NoSuchElementException:
-    print('No error page')
+    def test1(self):
+        time.sleep(1)
+        self._from = self.browser.find_element_by_id("from")
+        self._to = self.browser.find_element_by_id("to")
+        self._when = self.browser.find_element_by_id("when")
+        self._from.clear()
+        self._from.send_keys("Кемерово")
+        self._to.send_keys("Москва")
+        self._when.send_keys("7 июля")
+        self._when.send_keys(Keys.ENTER)        
+        time.sleep(2)
+        print("TEST 1:")
+        try:
+            self.header = self.browser.find_element_by_xpath("//html/body/div/div/main/div/div[1]/div[1]/div/section/article[1]/header/div/h3/a")
+            print("Header is displayed: " + self.header.text)
+        except NoSuchElementException:
+            print("No header")
+        time.sleep(2)
+        try:
+            self.duration = self.browser.find_element_by_xpath("//html/body/div/div/main/div/div[1]/div[1]/div/section/article[1]/div[1]/div[1]/div[2]")
+            print("Duration time is displayed: " + self.duration.text)
+        except NoSuchElementException:
+            print("No duration time")
+        try:
+            self.browser.find_element_by_tag_name("svg")
+            print("Icon is displayed")
+        except NoSuchElementException:
+            print("No icon")
+
+        search_segment = self.browser.find_elements_by_class_name("SearchSegment")
+        segments = len(search_segment)
+        if segments == 5:
+            print("5 segments")
+        else:
+            print("Not 5 segments")
+
+    def test2(self):
+        self._from = self.browser.find_element_by_id("from")
+        self._from.clear()
+        self._from.send_keys("Кемерово проспект Ленина")
+        self._to = self.browser.find_element_by_id("to")
+        self._to.send_keys("Кемерово Бакинский переулок")
+        self._when = self.browser.find_element_by_id("when")
+        self._when.send_keys("среда")
+        print("TEST 2:")
+        bus = self.browser.find_element_by_xpath("//*[@id='root']/div/header/div[1]/div/div[4]/span/label[5]")
+        bus.click()
+        search_button = self.browser.find_element_by_xpath("//*[@id='root']/div/header/div[1]/div/div[5]/form/button[2]")
+        search_button.click()
+        try:
+            self.browser.find_elements_by_class_name("ErrorPage")
+            print("Error page is displayed")
+        except NoSuchElementException:
+            print("No error page")
+
+if __name__ == "main":
+    unittest.main()
